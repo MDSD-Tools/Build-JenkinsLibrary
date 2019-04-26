e94ed5457335077f7327ca6ff2c997825d3af15b
Stephan Seifermann <stephan.seifermann@kit.edu>
2019-04-26 18:33:46 +0200

    Try

--- vars/buildPipeline.groovy
+++ vars/buildPipeline.groovy
@@ -242,55 +242,7 @@ def call(body) {
 		}
 		throw err
 	} finally {
-		def currentResult = currentBuild.result ?: 'SUCCESS'
-		def previousResult = currentBuild.previousBuild?.result ?: 'SUCCESS'
-
-		if (!skipNotification) {
-			if (currentResult == 'FAILURE') {
-				notifyFailure(MAIL_DEFAULT_RECIPIENT)
-			} else if (currentResult == 'SUCCESS' && previouslyFailed()) {
-				notifyFix(MAIL_DEFAULT_RECIPIENT)
-			}
-		}
+		emailNotification(MAIL_DEFAULT_RECIPIENT)
 	}
 
-}
-
-def notifyFix(defaultRecipient) {
-	notify(defaultRecipient, 'FIXED', 'fixed previous build errors')
-}
-
-def notifyFailure(defaultRecipient) {
-	notify(defaultRecipient, 'FAILED', 'failed')
-}
-
-def notify(defaultRecipient, token, verb) {
-	node {
-		wrap([$class: 'MaskPasswordsBuildWrapper', varMaskRegexes: [
-			[regex: '@[^\\s,]+']
-		]]) {
-			emailext([
-				attachLog: true,
-				body: "The build of ${JOB_NAME} #${BUILD_NUMBER} ${verb}.\nPlease visit ${BUILD_URL} for details.",
-				subject: "${token}: build of ${JOB_NAME} #${BUILD_NUMBER}",
-				to: defaultRecipient,
-				recipientProviders: [[$class: 'RequesterRecipientProvider'], [$class:'CulpritsRecipientProvider']]
-			])
-		}
-	}
-
-}
-
-def previouslyFailed() {
-	for (buildObject = currentBuild.previousBuild; buildObject != null; buildObject = buildObject.previousBuild) {
-		if (buildObject.result == null) {
-			continue
-		}
-		if (buildObject.result == 'FAILURE') {
-			return true
-		}
-		if (buildObject.resultIsBetterOrEqualTo('UNSTABLE')) {
-			return false
-		}
-	}
 }
