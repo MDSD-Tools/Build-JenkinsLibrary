f9c428764c415a0685242ee754033204076d9950
Sebastian Krach <krach@fzi.de>
2020-04-23 11:00:10 +0200

    Adapted reading of properties file

--- resources/tools/mdsd/devops/pipeline/stages/impl/dockerized/gradle/modules/Container/SetupContainer.groovy
+++ resources/tools/mdsd/devops/pipeline/stages/impl/dockerized/gradle/modules/Container/SetupContainer.groovy
@@ -5,21 +5,24 @@ def gradleJdkVersion = CFG.gradleJdkVersion ?: '11'
 // We do not use the gradle wrapper directly, to avoid executing binary code from the repository and benefit from
 // caching of the build container images.
 if (!CFG.gradleVersion) {
-    def propFile = new File("${CFG.workspacePath}/gradle/wrapper/gradle-wrapper.properties")
-    if (propFile.exists() && propFile.canRead()) {
-        def wrapperProperties = new Properties()
-        propFile.withInputStream {
-            stream -> wrapperProperties.load(stream)
-        }
+    if (fileExists('gradle/wrapper/gradle-wrapper.properties')) {
+        def wrapperProperties = readProperties file: 'gradle/wrapper/gradle-wrapper.properties'
         if (wrapperProperties.containsKey("distributionUrl")) {
             def url = wrapperProperties["distributionUrl"]
             // Use regex to extract Gradle version from distribution url
             // The url is usually formatted as follows: https\://services.gradle.org/distributions/gradle-5.6.4-all.zip
             def match = url =~ /\d+\.\d+\.\d+/
             if (match.find()) {
+                echo "Version extracted from Gradle wrapper: ${match[0]}"
                 gradleVersion = match[0]
+            } else {
+                echo "Could not extract version from Gradle distribution URL $url. Fallback to default version."
             }
+        } else {
+            echo "gradle-wrapper.properties does not specify a distributionUrl. Fallback to default version."
         }
+    } else {
+        echo "No Gradle version specified and no Gradle wrapper present. Fallback to default version."
     }
 }
 
