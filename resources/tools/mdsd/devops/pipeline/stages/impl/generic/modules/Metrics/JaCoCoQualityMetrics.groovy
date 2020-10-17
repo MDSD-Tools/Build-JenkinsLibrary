e03010a842624a9502ef9a3bf55506d4dd9ea1a0
Stephan Seifermann <stephan.seifermann@kit.edu>
2020-10-17 21:46:47 +0200

    Added logic to compare job coverage.

--- resources/tools/mdsd/devops/pipeline/stages/impl/generic/modules/Metrics/JaCoCoQualityMetrics.groovy
+++ resources/tools/mdsd/devops/pipeline/stages/impl/generic/modules/Metrics/JaCoCoQualityMetrics.groovy
@@ -10,17 +10,41 @@ jacoco([
 ])
 
 if (CFG.isPullRequest) {
-    echo "Test Coverage Debug"
-    def lastCoverageUrl = "${env.JOB_URL}../main/lastSuccessfulBuild/jacoco/api/json"
-    def response = httpRequest url: lastCoverageUrl, validResponseCodes: '100:599'
+    final COVERAGE_LEVEL = 'instructionCoverage'
+    final EPSILON = 0.1
+
+
+    echo "Collecting coverage of master branch"
+    def masterCoverageUrl = "${env.JOB_URL}../main/lastSuccessfulBuild/jacoco/api/json"
+    def response = httpRequest url: masterCoverageUrl, validResponseCodes: '100:599'
     if (!200.equals(response.status)) {
-        lastCoverageUrl = "${env.JOB_URL}../master/lastSuccessfulBuild/jacoco/api/json"
-        response = httpRequest url: lastCoverageUrl, validResponseCodes: '100:599'
+        masterCoverageUrl = "${env.JOB_URL}../master/lastSuccessfulBuild/jacoco/api/json"
+        response = httpRequest url: masterCoverageUrl, validResponseCodes: '100:599'
     }
     if (200.equals(response.status)) {
         def jsonObject = readJSON text: response.content
-        def coverage = jsonObject['instructionCoverage']['percentageFloat']
-        echo "${coverage}"
+        def masterCoverage = jsonObject[COVERAGE_LEVEL]['percentageFloat']
+        echo "Found master branch coverage ${COVERAGE_LEVEL} of ${masterCoverage}."
+
+        echo "Determining job coverage"
+        def coverageUrl = "${env.BUILD_URL}/jacoco/api/json"
+        response = httpRequest url: coverageUrl, validResponseCodes: '100:599'
+        if (200.equals(response.status)) {
+            jsonObject = readJSON text: response.content
+            def jobCoverage = jsonObject[COVERAGE_LEVEL]['percentageFloat']
+            echo "Found job coverage ${COVERAGE_LEVEL} of ${jobCoverage}."
+            if (jobCoverage - masterCoverage < -1.0 * EPSILON) {
+                echo "Coverage decreased from ${masterCoverage} to ${jobCoverage}"
+                // TODO report worsened coverage
+            } else {
+                echo "Coverage did not decrease."
+                // TODO report increased or same coverage
+            }
+        } else {
+            echo "Could not find job coverage."
+            // TODO report worsened coverage
+        }
+    } else {
+        echo "Could not find master branch coverage. Skipping coverage comparison."
     }
-    echo "${response.status}"
 }
