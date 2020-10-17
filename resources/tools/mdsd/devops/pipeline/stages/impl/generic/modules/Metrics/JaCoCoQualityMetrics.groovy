c264f892f0243dd5e5a827fa7e6430dc8ee1af7e
Stephan Seifermann <stephan.seifermann@kit.edu>
2020-10-17 22:57:08 +0200

    Add epsilon check.

--- resources/tools/mdsd/devops/pipeline/stages/impl/generic/modules/Metrics/JaCoCoQualityMetrics.groovy
+++ resources/tools/mdsd/devops/pipeline/stages/impl/generic/modules/Metrics/JaCoCoQualityMetrics.groovy
@@ -41,42 +41,54 @@ if (CFG.isPullRequest) {
         def masterCoverage = jsonObject[COVERAGE_LEVEL]['percentageFloat']
         echo "Found master branch coverage ${COVERAGE_LEVEL} of ${masterCoverage}."
 
-        // search for job coverage
-        echo "Determining job coverage"
-        def coverageUrl = "${env.BUILD_URL}jacoco/api/json"
-        response = httpRequest url: coverageUrl, validResponseCodes: '100:599'
-        if (200.equals(response.status)) {
-            jsonObject = readJSON text: response.content
-            def jobCoverage = jsonObject[COVERAGE_LEVEL]['percentageFloat']
-            echo "Found job coverage ${COVERAGE_LEVEL} of ${jobCoverage}."
+        // only consider reference coverages greater than epsilon
+        if (masterCoverage > EPSILON) {
 
-            // comparison of job with master coverage
-            if (jobCoverage - masterCoverage < -1.0 * EPSILON) {
+            // search for job coverage
+            echo "Determining job coverage"
+            def coverageUrl = "${env.BUILD_URL}jacoco/api/json"
+            response = httpRequest url: coverageUrl, validResponseCodes: '100:599'
+            if (200.equals(response.status)) {
+                jsonObject = readJSON text: response.content
+                def jobCoverage = jsonObject[COVERAGE_LEVEL]['percentageFloat']
+                echo "Found job coverage ${COVERAGE_LEVEL} of ${jobCoverage}."
 
-                // coverage has worsened
-                echo "Coverage decreased from ${masterCoverage} to ${jobCoverage}"
-                githubNotify credentialsId: CREDENTIALS_ID,
-                             context: STATUS_CONTEXT,
-                             targetUrl: STATUS_URL,
-                             status: 'ERROR',
-                             description: "The code coverage decreased from ${masterCoverage}% to ${jobCoverage}% with respect to the ${masterBranchName} branch."
+                // comparison of job with master coverage
+                if (jobCoverage - masterCoverage < -1.0 * EPSILON) {
+
+                    // coverage has worsened
+                    echo "Coverage decreased from ${masterCoverage} to ${jobCoverage}"
+                    githubNotify credentialsId: CREDENTIALS_ID,
+                                context: STATUS_CONTEXT,
+                                targetUrl: STATUS_URL,
+                                status: 'ERROR',
+                                description: "The code coverage decreased from ${masterCoverage}% to ${jobCoverage}% with respect to the ${masterBranchName} branch."
+                } else {
+                    // coverage is better or equal
+                    echo "Coverage did not decrease."
+                    githubNotify credentialsId: CREDENTIALS_ID,
+                                context: STATUS_CONTEXT,
+                                targetUrl: STATUS_URL,
+                                status: 'SUCCESS',
+                                description: "The code coverage did not decrease with respect to the ${masterBranchName} branch."
+                }
             } else {
-                // coverage is better or equal
-                echo "Coverage did not decrease."
+                // no job coverage found
+                echo "Could not find job coverage."
                 githubNotify credentialsId: CREDENTIALS_ID,
-                             context: STATUS_CONTEXT,
-                             targetUrl: STATUS_URL,
-                             status: 'SUCCESS',
-                             description: "The code coverage did not decrease with respect to the ${masterBranchName} branch."
+                            context: STATUS_CONTEXT,
+                            targetUrl: STATUS_URL,
+                            status: 'FAILURE',
+                            description: "Code coverage of job could not be determined but there is a coverage for the ${masterBranchName} branch."
             }
         } else {
-            // no job coverage found
-            echo "Could not find job coverage."
+            // master coverage is too low to be considered
+            echo "Master coverage is below threshold."
             githubNotify credentialsId: CREDENTIALS_ID,
-                         context: STATUS_CONTEXT,
-                         targetUrl: STATUS_URL,
-                         status: 'FAILURE',
-                         description: "Code coverage of job could not be determined but there is a coverage for the ${masterBranchName} branch."
+                        context: STATUS_CONTEXT,
+                        targetUrl: STATUS_URL,
+                        status: 'SUCCESS',
+                        description: "There is no significant code coverage for the ${masterBranchName} branch, skipping coverage check."
         }
     } else {
         // no master coverage found
