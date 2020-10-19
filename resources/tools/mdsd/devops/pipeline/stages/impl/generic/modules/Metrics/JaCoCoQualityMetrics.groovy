a0a6630278ad520ce9a609a2410ebc529003d2fa
Stephan Seifermann <9149072+seiferma@users.noreply.github.com>
2020-10-19 13:47:25 +0200

    Add code coverage status test in pipeline (#23)

--- resources/tools/mdsd/devops/pipeline/stages/impl/generic/modules/Metrics/JaCoCoQualityMetrics.groovy
+++ resources/tools/mdsd/devops/pipeline/stages/impl/generic/modules/Metrics/JaCoCoQualityMetrics.groovy
@@ -8,3 +8,95 @@ jacoco([
     inclusionPattern: '**/*.class',
     exclusionPattern: '**/*Test*.class'
 ])
+
+// perform code coverage status check for pull requests
+if (CFG.isPullRequest) {
+
+    // constants
+    final COVERAGE_LEVEL = 'instructionCoverage'
+    final EPSILON = 0.001
+    final STATUS_CONTEXT = 'continuous-integration/jenkins/codecoverage'
+    final STATUS_URL = "${env.BUILD_URL}jacoco"
+    final CREDENTIALS_ID = '8adf889c-2157-45d1-acc7-1c6211538dac'
+
+    // pending notification
+    githubNotify credentialsId: CREDENTIALS_ID,
+                 context: STATUS_CONTEXT,
+                 targetUrl: STATUS_URL,
+                 status: 'PENDING',
+                 description: "Status check in progress..."
+
+    // search for master/main code coverage
+    echo "Collecting coverage of master branch"
+    def masterBranchName = 'main'
+    def masterCoverageUrl = "${env.JOB_URL}../${masterBranchName}/lastSuccessfulBuild/jacoco/api/json"
+    def response = httpRequest url: masterCoverageUrl, validResponseCodes: '100:599'
+    if (!200.equals(response.status)) {
+        masterBranchName = 'master'
+        masterCoverageUrl = "${env.JOB_URL}../${masterBranchName}/lastSuccessfulBuild/jacoco/api/json"
+        response = httpRequest url: masterCoverageUrl, validResponseCodes: '100:599'
+    }
+    if (200.equals(response.status)) {
+        def jsonObject = readJSON text: response.content
+        def masterCoverage = jsonObject[COVERAGE_LEVEL]['percentageFloat']
+        echo "Found master branch coverage ${COVERAGE_LEVEL} of ${masterCoverage}."
+
+        // only consider reference coverages greater than epsilon
+        if (masterCoverage > EPSILON) {
+
+            // search for job coverage
+            echo "Determining job coverage"
+            def coverageUrl = "${env.BUILD_URL}jacoco/api/json"
+            response = httpRequest url: coverageUrl, validResponseCodes: '100:599'
+            if (200.equals(response.status)) {
+                jsonObject = readJSON text: response.content
+                def jobCoverage = jsonObject[COVERAGE_LEVEL]['percentageFloat']
+                echo "Found job coverage ${COVERAGE_LEVEL} of ${jobCoverage}."
+
+                // comparison of job with master coverage
+                if (jobCoverage - masterCoverage < -1.0 * EPSILON) {
+
+                    // coverage has worsened
+                    echo "Coverage decreased from ${masterCoverage} to ${jobCoverage}"
+                    githubNotify credentialsId: CREDENTIALS_ID,
+                                context: STATUS_CONTEXT,
+                                targetUrl: STATUS_URL,
+                                status: 'ERROR',
+                                description: "The code coverage decreased from ${masterCoverage}% to ${jobCoverage}% with respect to the ${masterBranchName} branch."
+                } else {
+                    // coverage is better or equal
+                    echo "Coverage did not decrease."
+                    githubNotify credentialsId: CREDENTIALS_ID,
+                                context: STATUS_CONTEXT,
+                                targetUrl: STATUS_URL,
+                                status: 'SUCCESS',
+                                description: "The code coverage did not decrease with respect to the ${masterBranchName} branch."
+                }
+            } else {
+                // no job coverage found
+                echo "Could not find job coverage."
+                githubNotify credentialsId: CREDENTIALS_ID,
+                            context: STATUS_CONTEXT,
+                            targetUrl: STATUS_URL,
+                            status: 'FAILURE',
+                            description: "Code coverage of job could not be determined but there is a coverage for the ${masterBranchName} branch."
+            }
+        } else {
+            // master coverage is too low to be considered
+            echo "Master coverage is below threshold."
+            githubNotify credentialsId: CREDENTIALS_ID,
+                        context: STATUS_CONTEXT,
+                        targetUrl: STATUS_URL,
+                        status: 'SUCCESS',
+                        description: "There is no significant code coverage for the ${masterBranchName} branch, skipping coverage check."
+        }
+    } else {
+        // no master coverage found
+        echo "Could not find master branch coverage. Skipping coverage comparison."
+        githubNotify credentialsId: CREDENTIALS_ID,
+                     context: STATUS_CONTEXT,
+                     targetUrl: STATUS_URL,
+                     status: 'SUCCESS',
+                     description: "There is no code coverage for the ${masterBranchName} branch, skipping coverage check."
+    }
+}
