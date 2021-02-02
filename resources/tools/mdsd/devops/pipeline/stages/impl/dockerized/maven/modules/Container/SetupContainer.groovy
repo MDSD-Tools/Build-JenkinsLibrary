815a46a1b7b2f0ffdb76d0d89a9acb4ee0a18a22
Stephan Seifermann <stephan.seifermann@kit.edu>
2021-02-02 17:24:07 +0100

    Use forked maven image if available.

--- resources/tools/mdsd/devops/pipeline/stages/impl/dockerized/maven/modules/Container/SetupContainer.groovy
+++ resources/tools/mdsd/devops/pipeline/stages/impl/dockerized/maven/modules/Container/SetupContainer.groovy
@@ -1,8 +1,18 @@
 def mavenVersion = CFG.mavenVersion ?: '3'
 def mavenJdkVersion = CFG.mavenJdkVersion ?: '11'
 
+// try to use a forked version of the maven image
+def mavenImageName = "maven:${mavenVersion}-jdk-${mavenJdkVersion}"
+def mavenImageFqn = "docker.mdsd.tools/${mavenImageName}"
+try{
+    def dockerImage = docker.image(mavenImageFqn)
+    dockerImage.pull()
+} catch (err){
+    mavenImageFqn = mavenImageName
+}
+
 extendConfiguration([
-    dockerBuildImage: "maven:${mavenVersion}-jdk-${mavenJdkVersion}",
+    dockerBuildImage: mavenImageFqn,
     dockerWithRunParameters: """\
     ${CFG.dockerWithRunParameters ?: ""} \
     -v ${CFG.slaveHome}/.m2:/.m2:ro \
