4b05ac6ee9c93ab6450b43c635fb106f227a5059
Stephan Seifermann <stephan.seifermann@kit.edu>
2021-03-20 14:39:49 +0100

    PALLADIO-505 Do not use HTTP proxy anymore.

--- resources/tools/mdsd/devops/pipeline/stages/impl/dockerized/modules/Container/SetupContainer.groovy
+++ resources/tools/mdsd/devops/pipeline/stages/impl/dockerized/modules/Container/SetupContainer.groovy
@@ -2,7 +2,6 @@ extendConfiguration([dockerWithRunParameters: """\
         ${CFG.dockerWithRunParameters ?: ""} \
         -u ${CFG.slaveUid} \
         -v ${CFG.workspacePath}:/ws:ro \
-        --network proxy \
         -it \
         --entrypoint=/bin/cat \
         """])
